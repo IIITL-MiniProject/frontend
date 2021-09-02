2dab2eb96984f3c1ecfbfd717827845872c2b04b
peaceDude404 <lcs2019012@iiitl.ac.in>
2021-09-02 21:54:52 +0530

    signup page added

---
+++ lib/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kbackground = const Color(0xffEEEEEE);
+const kprimarytheme = const Color(0xff50C2C9);
+const kgreen = const Color(0xff50C2C9);
+const kLabelTextStyle=TextStyle(
+  fontSize: 22,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+final String circlex = 'assets/icons/circle-shape.svg';
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frontend/constant.dart';
+import 'Components/signup_form.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          backgroundColor: Theme.of(context).accentColor,
+          body: SingleChildScrollView(
+                      child: Column(
+        children: [
+                Row(
+                  children: [
+                         SvgPicture.asset(circlex,),
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Text('Welcome Onboard !!',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                SizedBox(height: 62),
+                SignupForm(),
+        ],
+      ),
+          ),
+    );
+  }
+}
